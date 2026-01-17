0b67ef6b857f7ba5b3696b119f7f2bfcefe0407d
herzelc <herzelc@gmail.com>
2026-01-17 20:11:44 +0200

    Update Ahalan on main

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 
 def greet(name):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    return f"Hello, {name}! The current time is {current_time}."
+    return f"Ahalan , {name}! The current time is {current_time}."
 
 if __name__ == "__main__":
     user = "World"

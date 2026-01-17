8c8ba3deeaa2d35b8b53affe2159dd3d0c0679db
herzelc <herzelc@gmail.com>
2026-01-17 21:45:07 +0200

    Main branch update: change user name to Herzel

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import datetime
 
 def greet(name):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-<<<<<<< HEAD
 
-    return f"Shalom!!, {name}! The current time is {current_time}."
-=======
-    return f"Hello, {name}! The current time is {current_time}."
->>>>>>> parent of 0b8c81e (Update Hello to Shalom on main)
+    return f"Shalom!!, {Herzel Cohen}! The current time is {current_time}."
+
+
 
 
 if __name__ == "__main__":

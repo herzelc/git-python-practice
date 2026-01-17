21822dca40d0c5c8abbbbddcec5528d0c9b71cb4
herzelc <herzelc@gmail.com>
2026-01-17 20:52:37 +0200

    Add a minor comment to app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import datetime
-# only temp message!!!
+# only 1 message!!!
 
 def greet(name):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
+<<<<<<< HEAD
 
     return f"Shalom!!, {name}! The current time is {current_time}."
+=======
+    return f"Hello, {name}! The current time is {current_time}."
+>>>>>>> parent of 0b8c81e (Update Hello to Shalom on main)
 
 >>>>>>> feature/add-time
 
